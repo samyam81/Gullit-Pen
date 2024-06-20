ba991bbf77910ed3243058b2f13d0173a7455527
samyam81 <samyam081@gmail.com>
2024-06-20 11:41:29 +0545

    Go

--- Main.go
+++ Main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 func main() {
-	http.HandleFunc("/GullitPen", servePen)
+    http.HandleFunc("/GullitPen", servePen)
+    http.HandleFunc("/seven20", serveSeven20)
 
-	fs := http.FileServer(http.Dir("./Static"))
-	http.Handle("/", http.StripPrefix("/", fs))
+    fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/", http.StripPrefix("/", fs))
 
-	fmt.Println("Server is running on http://localhost:8080")
-
-	http.ListenAndServe(":8080", nil)
+    fmt.Println("Server is running on http://localhost:8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
 
-// servePen handles requests to /GullitPen by serving seven.html
 func servePen(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./Static/seven.html")
+    http.ServeFile(w, r, "./static/seven.html")
+}
+
+func serveSeven20(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven20.html")
 }
