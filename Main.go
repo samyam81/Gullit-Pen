8f647a0b3f9862b984b67416b414dfb931ca6026
samyam81 <samyam081@gmail.com>
2024-06-20 11:49:46 +0545

    New Function

--- Main.go
+++ Main.go
@@ -7,7 +7,15 @@ import (
 
 func main() {
     http.HandleFunc("/GullitPen", servePen)
+
     http.HandleFunc("/seven20", serveSeven20)
+	http.HandleFunc("/seven2",serveSeven2)
+
+	http.HandleFunc("/seven",serveSeven)
+	http.HandleFunc("/seven0",serveSeven0)
+
+	http.HandleFunc("/seven1",serveSeven1)
+	http.HandleFunc("/seven10",serveSeven10)
 
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/", http.StripPrefix("/", fs))
@@ -25,4 +33,24 @@ func servePen(w http.ResponseWriter, r *http.Request) {
 
 func serveSeven20(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "./static/seven20.html")
+}
+
+func serveSeven2(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven2.html")
+}
+
+func serveSeven(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven.html")
+}
+
+func serveSeven0(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven0.html")
+}
+
+func serveSeven10(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven10.html")
+}
+
+func serveSeven1(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "./static/seven1.html")
 }
